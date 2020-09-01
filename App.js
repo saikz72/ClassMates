3e6f99c2f5dcd34134ef4e45288cc82b21abc878
Saikou Ceesay <ceesaysaikou72@gmail.com>
2020-08-31 21:09:51 -0400

    initialized

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text, View } from 'react-native';
 
 const App = () => {
-  return <Text>Welcome to Support Room</Text>;
+  return (
+    <View>
+      <Text>Welcome to support room</Text>
+    </View>
+  );
 };
 
 export default App;
