bbce314d52efb20dd39c80b3f94d9c6d77162a06
Derah Onuorah <derahonuorah@Derahs-MacBook-Pro.local>
2020-09-04 19:19:56 -0400

    Comments to LaunchScreen

--- src/screens/LaunchScreen.js
+++ src/screens/LaunchScreen.js
@@ -3,31 +3,31 @@ import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
 const LaunchScreeen = () => {
   return (
+    // View for the whole screen that puts the logo right in the center of the screen
     <View style = {styles.fullPage}>
       <View>
-        <Image source = {require("../../assets/logo.png")} style={styles.image}/>
+        <Image source = {require("../../assets/logo.png")} style={styles.logo}/>
       </View>
     </View>  
   )
 };
 
 const styles = StyleSheet.create ({
-  image:{
+  //styling for:
+  
+  //the logo
+  logo:{
     height:  150,
     width: 190,
     alignSelf: 'center'
   },
+
+  //the page/background
   fullPage:{
     flex: 1,
     alignItems: 'stretch',
     justifyContent: 'center',
     backgroundColor: 'white'
-  },
-  button:{
-    borderColor: 'blue',
-    borderWidth: 1,
-    borderRadius: 10,
-    alignItems: 'center',
   },
 });
 
